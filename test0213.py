af4d2b7da11abd0c3676ca76dd928a84487cbdd3
llorz <jing.ren@kaust.edu.sa>
2019-02-18 21:48:36 +0300

    update test0213

--- test0213.py
+++ test0213.py
@@ -15,13 +15,9 @@ S2 = MESH.mesh_load_and_preprocess(DATASET_PATH + "tr_reg_001.off",
                                    numEigs=50)
 
 desc1 = fMap.wave_kernel_signature(S1.evecs, S1.evals, S1.A, numTimes=100, ifNormalize=True)
-desc1 = desc1[:, np.linspace(0, 100, 20)]
+desc1 = desc1[:, np.arange(0,100,20)]
 desc2 = fMap.wave_kernel_signature(S1.evecs, S1.evals, S1.A, numTimes=100, ifNormalize=True)
-desc2 = desc2[:, np.linspace(0, 100, 20)]
+desc2 = desc2[:, np.arange(0,100,20)]
 
-
-print(desc1.shape)
-#B = S.evecs[:, 0:10]
-#OpSrc = fMap.descriptor_commutativity_operator(desc, B, S.A)
-
-
+B1 = S1.evecs
+B2 = S2.evecs
